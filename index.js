4214f5bf124d858fa26fc3700f58aa0bea666266
Tyson Romero <romennoodles23@yahoo.com>
2021-09-06 15:06:42 -0600

    just an updated ES6 way to do same function

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+// const searchInput = document.querySelector("#searchInput")
+// const namesFromDOM = document.querySelectorAll(".name")
+// console.log(namesFromDOM)
+
+// searchInput.addEventListener('keyup', function (event) {
+//     const { value } = event.target
+
+//     const searchQuery = value.toLowerCase()
+
+//     for (const nameElement of namesFromDOM) {
+//         let name = nameElement.textContent.toLowerCase()
+
+//         if (name.includes(searchQuery)) {
+//             nameElement.style.display = 'block'
+//         } else {
+//             nameElement.style.display = 'none'
+//         }
+//     }
+// })
+
 const searchInput = document.querySelector("#searchInput")
 const results = document.querySelector("#results")
 const allNamesDOMCollection = document.querySelectorAll(".name")
